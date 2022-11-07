0cc972f138070a942ce770d5c3cf44888d91ee25
Matheus Fidelis <matheus@nanoshots.com.br>
2022-11-06 21:24:20 -0300

    field notes: kmeans

--- rlang/clustering/kmeans.R
+++ rlang/clustering/kmeans.R
@@ -1,6 +1,4 @@
-##### Teste de Clustering de Dados - Kmeans
-
-# Exemplo 1
+##### Teste de Clustering de Dados
 
 # O exemplo é encontrar uma familiariade entre alunos que possuem uma correlação
 # entre notas de Matemática e Física 
@@ -9,7 +7,8 @@
 
 pacotes <- c(
              "cluster", 
-             "factoextra"
+             "factoextra",
+             "dplyr"
             )
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
@@ -43,6 +42,7 @@ base_cluster_vars
 ##### Buscando o numero ideal de Clusters para a amostra
 fviz_nbclust(base_cluster_vars, kmeans, method = "gap_stat")
 
+
 ##### Criando os clusters 
 clus <- kmeans(base_cluster_vars, 3)
 
@@ -67,3 +67,50 @@ melhores_alunos_exatas
 
 piores_alunos_exatas <- subset(base_original, cluster == "3")
 piores_alunos_exatas 
+
+
+##### Exemplo 2: Correlação entre mortalidade infantil e inflação nos países
+
+##### Carregando o Dataset
+paises <- read.csv("dados_paises.csv", sep = ",", dec = ".")
+paises
+
+paises_original <- paises
+paises_cluster <- paises
+
+###### Selecionando apenas as colunas de inflação e mortalidade para o dataset
+
+paises_cluster <- select(paises_cluster, child_mort, inflation)
+paises_cluster
+
+####### Transformando os dados para a mesma escala
+paises_cluster <- scale(paises_cluster)
+paises_cluster
+
+####### Transformando a escala para um dataframe
+paises_cluster <- as.data.frame(paises_cluster)
+paises_cluster
+
+##### Buscando o numero ideal de Clusters para a amostra
+fviz_nbclust(paises_cluster, kmeans, method = "gap_stat") 
+
+# RES: 2
+##### Criando os clusters 
+paises_kmens_cluster <- kmeans(paises_cluster, 3)
+
+##### Visualizando os clusters
+fviz_cluster(paises_kmens_cluster, data = paises_cluster)
+
+##### Combinação da informação do cluster na base original
+paises_original <- cbind(paises_original, paises_kmens_cluster$cluster)
+paises_original
+
+##### Renomeando a varieavel do cluster 
+names(paises_original)[11] <- 'cluster'
+paises_original
+
+
+#### Criando um dataset com os paises com maior correlação entre inflação x mortalidade 
+
+maior_mortalidade <- subset(paises_original, cluster == "3")
+maior_mortalidade 
