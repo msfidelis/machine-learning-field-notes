fade62ac6e3dec83c5d7c6a60fd3bde7c44a5dbf
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-11 10:36:09 -0300

    Correlações de Person, Spearman e Kendall

--- rlang/correlacoes/correlacoes.R
+++ rlang/correlacoes/correlacoes.R
@@ -10,6 +10,71 @@ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   sapply(pacotes, require, character = T)
 }
 
+# COLA DE CORRELAÇÕES QUANTITATIVAS
+
+## A correlação é uma análise bivariada que mede a força de associação entre duas 
+## variáveis e a direção da relação.  Em termos da força da relação, 
+## o valor do coeficiente de correlação varia entre +1 e -1.  
+## Um valor de ± 1 indica um grau de associação perfeito entre as duas variáveis.
+## Como o valor do coeficiente de correlação vai para 0, a relação entre as duas 
+## variáveis será mais fraca.
+
+
+# TIPOS DE COEFIENTES DE CORRELAÇÃO 
+
+
+# PEARSON - CORRELAÇÃO LINEAR
+
+## Sejam duas variáveis X e Y, ambas quantitativas, preferencialmente contínuas. 
+## A existência de relação linear entre essas variáveis pode ser detectada 
+## com auxílio do Diagrama de Dispersão, mas, também, com auxílio do Coeficiente 
+## de Correlação Linear de Pearson.
+
+## A correlação de Pearson avalia a relação linear entre duas variáveis contínuas. 
+## Uma relação é linear quando a mudança em uma variável é associada a uma 
+## mudança proporcional na outra variável.
+
+## Por exemplo, você poderia usar uma correlação de Pearson para avaliar se 
+## aumentos na temperatura da instalação de produção estão associados a uma 
+## redução da espessura da cobertura de chocolate.
+
+## Utilizado quando: Temos normalidade dos dados, e eles são LINEARES
+
+
+
+# SPEARMAN - CORRELAÇÃO MONOTÔNICA
+
+## A correlação de Spearman avalia a relação monotônica entre duas variáveis 
+## contínuas ou ordinais. Em uma relação monotônica, as variáveis tendem a mudar 
+## juntas mas não necessariamente a uma taxa constante. O coeficiente de 
+## correlação de Spearman baseia-se nos valores classificados de cada variável, 
+## em vez de os dados brutos.
+
+## A correlação de Spearman é muito usada para avaliar relações envolvendo 
+## variáveis ordinais. Por exemplo, você poderia usar a correlação de Spearman 
+## para avaliar se a ordem na qual os funcionários executam um teste está 
+## relacionada ao número de meses de emprego.
+## Usado para amoastras maiores
+
+
+
+# KENDAL - CORRELAÇÃO ORDINAL
+
+## O coeficiente de correlação Tau de Kendall serve para verificar se existe 
+## correlação entre duas variáveis ordinais. É um método adequado quando 
+## amostras têm tamanhos reduzidos, pois o método é mais preciso. 
+## E pode ser estendido a correlações parciais, quando o efeito de uma 
+## terceira variável, que age sobre X e Y, é retirado antes de determinar 
+## se X e Y estão relacionadas.
+## Coeficiente de Kendall é, muitas vezes, interpretado como uma medida de 
+## concordância entre dois conjuntos de classificações relativas a um conjunto 
+## de objetos de estudo.
+
+## Parecido com Spearman, mas utilizado para amostras menores
+
+## Os números ordinais representam posições em uma determinada sequência, 
+## como: primeiro, segundo, décimo quinto, entre outros.
+
 
 ## Abrindo o arquivo inicial para efetuar análise
 idh_raw <- read.table("idh.csv",
@@ -34,14 +99,55 @@ inpc_raw <- read.table("inpc_recente.csv",
 inpc_raw
 
 # Unindo os Datasets de IPCA E INPC
-
 dataset_correlacao <- as.data.frame(ipca_raw$mes_ano)
 
 
 dataset_correlacao$inpc <- inpc_raw$variacao_mes
 dataset_correlacao$ipca <- ipca_raw$variacao_mes
 dataset_correlacao
-  
+
+# TESTANDO CORRELAÇÕES
+
+# TESTANDO SE AS CORRELAÇÕES SÃO SIGNIFICATIVAS
+
+## Pearson
+cor.test(
+  dataset_correlacao$ipca, 
+  dataset_correlacao$inpc, 
+  method = "pearson"
+)
+
+cor.test(
+  idh_comparacao$expectativa_de_vida, 
+  idh_comparacao$expectativa_de_anos_escola, 
+  method = "spearman"
+)
+
+## Spearman
+cor.test(
+  dataset_correlacao$ipca, 
+  dataset_correlacao$inpc, 
+  method = "spearman"
+)
+
+cor.test(
+  idh_comparacao$expectativa_de_vida, 
+  idh_comparacao$expectativa_de_anos_escola, 
+  method = "spearman"
+)
+
+## Kendall
+cor.test(
+  dataset_correlacao$ipca, 
+  dataset_correlacao$inpc, 
+  method = "kendall"
+)
+
+cor.test(
+  idh_comparacao$expectativa_de_vida, 
+  idh_comparacao$expectativa_de_anos_escola, 
+  method = "kendall"
+)
 
 
 # Correlação Simples
@@ -89,9 +195,12 @@ pearson_inflacao$n
 
 
 # CORRELAÇÃO DE SPEARMAN 
+rcorr(as.matrix(dataset_correlacao[,2:3]), type="spearman")
 
 ## Correlação de todas as variáveis
-rcorr(as.matrix(dataset_correlacao[,2:3]), type="spearman")
+## inpc ipca
+## inpc 1.00 0.97
+## ipca 0.97 1.00
 
 ## Correlação entre dois tipos de variáveis
 x <- dataset_correlacao$ipca
@@ -139,8 +248,37 @@ rcorr(as.matrix(idh_comparacao[,2:3]), type="pearson")
 
 
 # GRAFICOS DE CORRELACAO
-
 corrgram(idh, order=TRUE, lower.panel=panel.shade,
          upper.panel=panel.pie, text.panel=panel.txt,
          main="Correlação entre INPC e IPCA")
+
+
+# HISTOGRAMAS DE DISTRIBUIÇÃO 
+
+histogram=function(x){
+  hist(x,prob=T)
+  lines(density(x),col="red")
+  curve(dnorm(x,mean(x), sd(x)),add=T,col="blue")
+}
+
+histogram(dataset_correlacao$ipca)
+histogram(dataset_correlacao$inpc)
+
+# VERIFICAÇÃO DE NORMALIDADES - QQPLOT
+
+qq = function(x){
+  qqnorm(x,main = "", xlab = "Quantis teóricos N(0,1)", pch = 20)
+  qqline(x, lty = 1, col = "red")
+}
+
+qq(dataset_correlacao$ipca)
+qq(dataset_correlacao$inpc)
+
+qq(idh$idh)
+qq(idh$idh_masculino)
+qq(idh$idh_feminino)
+qq(idh$expectativa_de_vida)
+qq(idh$expectativa_de_vida_masculina)
+qq(idh$expectativa_de_vida_feminina)
+
 
